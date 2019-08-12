09d8df84ddc8f1858c5000e12e31fc1a6371c6f0
fxneiram <53311660+fxneiram@users.noreply.github.com>
2019-08-12 12:55:14 -0500

    Delete installation.php

--- config/installation.php
+++
@@ -1,6 +0,0 @@
-<?php 
-                            return ['install' => true,
-                                          'version' => 1,
-                                          'install date' => '2019-08-08 14:07:45'
-                                        ];
-                       ?>
