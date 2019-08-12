e5660568910aeb9d3586d8f031d80a92e4674b29
fasanemo_48@hotmail.com <1121715761Faver>
2019-08-12 13:17:51 -0500

    Camboi del titulo a "SI ES POSIBLE"

--- resources/views/install/user.blade.php
+++ resources/views/install/user.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marama ERP - Creación del usuario administrador</title>
+    <title>SI ES POSIBLE - Creación del usuario administrador</title>
     <!-- CSS -->
     @include('install.partials.styles')
 </head>
